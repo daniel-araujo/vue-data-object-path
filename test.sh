c436331b75f27f8aa1a09d20df195d3f9fee1424
Daniel Araujo <contact@daniel-araujo.pt>
2020-04-29 22:45:02 +0100

    Fix sentence

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ vue_versions="~2.0.0 ~2.1.0 ~2.2.0 ~2.3.0 ~2.4.0 ~2.5.0 ~2.6.0"
 for vue_version in $vue_versions; do
   # The module of this version of vue is installed temporarily so that in case
   # the test fails you will be able to debug the code faster by directly
-  # executing the command that runs the unit tests instead of starting.
+  # executing the command that runs the unit tests.
   npm install --no-save "vue@$vue_version"
 
   # Runs unit tests against this version.
